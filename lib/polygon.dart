a18c3d6b201c78686b3e11cadaaa334d608c5e11
doodlezucc <electerminator@gmail.com>
2021-06-23 20:39:36 +0200

    added polygon bounding box getter

--- lib/polygon.dart
+++ lib/polygon.dart
@@ -1,5 +1,33 @@
 import 'dart:math';
 
 class Polygon {
-  final List<Point> points = [];
+  final List<Point<int>> _points;
+  final bool positive;
+
+  Polygon({List<Point<int>> points, this.positive}) : _points = points ?? [];
+
+  Rectangle<int> getBoundingBox() {
+    var p1 = _points.first;
+    var xMin = p1.x;
+    var xMax = p1.x;
+    var yMin = p1.y;
+    var yMax = p1.y;
+
+    for (var i = 1; i < _points.length; i++) {
+      var p = _points[i];
+      if (p.x < xMin) {
+        xMin = p.x;
+      } else if (p.x > xMax) {
+        xMax = p.x;
+      }
+
+      if (p.y < yMin) {
+        yMin = p.y;
+      } else if (p.y > yMax) {
+        yMax = p.y;
+      }
+    }
+
+    return Rectangle<int>(xMin, yMin, xMax - xMin, yMax - yMin);
+  }
 }
